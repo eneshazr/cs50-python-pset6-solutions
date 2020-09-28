9acf8bf4d135dd01fa786551333aeffaa52d139a
eneshazr <eneshazrr@gmail.com>
2020-09-28 23:08:38 +0300

    Create mario2.py

---
+++ mario2.py
@@ -0,0 +1,13 @@
+import cs50
+
+while True:
+
+    satir = cs50.get_int("Yukseklik: ")
+    if satir <= 0 or satir > 23:
+        continue
+
+    else:   
+        for i in range(satir):
+            print(' '*(satir-i-1) + '#'*(1*i+1),end="")
+            print(' ','#'*(1*i+1))
+        break
