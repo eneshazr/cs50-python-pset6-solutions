a76dd488f6f0fc29edd511e10ae459ccf75f7d7e
eneshazr <eneshazrr@gmail.com>
2020-09-28 23:46:42 +0300

    Create cash.py

---
+++ cash.py
@@ -0,0 +1,17 @@
+from cs50 import get_float
+from math import floor
+
+
+while True:
+    dollar = get_float("Change Owed: ")
+    cent = floor(dollar * 100)
+
+    if cent > 0:
+        break
+
+ceyrek = cent // 25
+cent10 = (cent % 25) // 10
+cent5 = ((cent % 25) % 10) // 5
+kurus = ((cent % 25) % 10) % 5
+
+print(f"{ceyrek + cent10 + cent5 + kurus}")
