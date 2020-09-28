a2292b7bf3a2b72245597cf4c667d7505ec4cf75
eneshazr <eneshazrr@gmail.com>
2020-09-28 23:47:41 +0300

    Update cash.py

--- cash.py
+++ cash.py
@@ -1,7 +1,6 @@
 from cs50 import get_float
 from math import floor
 
-
 while True:
     dollar = get_float("Change Owed: ")
     cent = floor(dollar * 100)
@@ -13,5 +12,4 @@ ceyrek = cent // 25
 cent10 = (cent % 25) // 10
 cent5 = ((cent % 25) % 10) // 5
 kurus = ((cent % 25) % 10) % 5
-
 print(f"{ceyrek + cent10 + cent5 + kurus}")
